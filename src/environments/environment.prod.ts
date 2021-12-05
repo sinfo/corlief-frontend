38c3476c0cbf298e4fb0b8d164618c8306c84403
Pedro Maximino <pedro.maximino@rnl.tecnico.ulisboa.pt>
2021-12-05 01:22:39 +0000

    Updated corlief api url in new infra.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  corlief: 'https://api.corlief.sinfo.org',
+  corlief: 'https://corlief.sinfo.org/api',
   deck: 'https://deck.sinfo.org',
   frontend: 'https://corlief.sinfo.org'
 };
