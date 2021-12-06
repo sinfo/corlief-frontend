901530163aaa7422daa8846b04ad441631091a07
Pedro Maximino <pedro.maximino@rnl.tecnico.ulisboa.pt>
2021-12-06 17:10:04 +0000

    Changed backend address.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  corlief: 'https://corlief.sinfo.org/api',
+  corlief: 'https://corlief.sinfo.org/api/secure',
   deck: 'https://deck.sinfo.org',
   frontend: 'https://corlief.sinfo.org'
 }
