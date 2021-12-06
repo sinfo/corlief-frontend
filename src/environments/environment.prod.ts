4651b1fc38f663d65781ec2894d9131f0f77a342
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2021-12-06 00:53:24 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  corlief: 'https://corlief2.sinfo.org/api',
+  corlief: 'https://corlief.sinfo.org/api',
   deck: 'https://deck.sinfo.org',
   frontend: 'https://corlief.sinfo.org'
 }
