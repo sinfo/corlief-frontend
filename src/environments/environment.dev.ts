0622766cb030b48063928bba30f2f68bf4c2f099
Pedro Maximino <pedro.maximino@rnl.tecnico.ulisboa.pt>
2022-11-25 15:54:41 +0000

    feat: Add dev environment to corlief

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    corlief: 'https://corlief-staging.sinfo.org/api/secure',
+    deck: 'https://deck-staging.sinfo.org',
+    frontend: 'https://corlief-staging.sinfo.org'
+  }
+  
