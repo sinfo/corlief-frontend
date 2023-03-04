bc876df6693a587298447500c2b7899849209c41
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-03-04 16:20:13 +0000

    fix: Change deck url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   corlief: 'https://corlief.sinfo.org/api/secure',
-  deck: 'https://deck.sinfo.org',
+  deck: 'https://deck-old.sinfo.org',
   frontend: 'https://corlief.sinfo.org'
 }
