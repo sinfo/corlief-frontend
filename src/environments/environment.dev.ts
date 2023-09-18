847e9ef5c8a31102bc2d196f8977c45c59245346
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-09-18 21:14:32 +0200

    fix: Add google clientId to all environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,9 @@ export const environment = {
     production: true,
     corlief: 'https://corlief-staging.sinfo.org/api/secure',
     deck: 'https://deck2.sinfo.org',
-    frontend: 'https://corlief-staging.sinfo.org'
+    frontend: 'https://corlief-staging.sinfo.org',
+    google: {
+      clientId: '475922911787-2eunqihjt791ul7kfi1ji185o7mlehq2.apps.googleusercontent.com'
+    },
   }
   
