e81dbc23c2eaad065a6e0d691527836e1f010059
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2023-09-18 20:14:37 +0200

    fix: Change deck staging link to new deck

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     corlief: 'https://corlief-staging.sinfo.org/api/secure',
-    deck: 'https://deck-staging.sinfo.org',
+    deck: 'https://deck2.sinfo.org',
     frontend: 'https://corlief-staging.sinfo.org'
   }
   
